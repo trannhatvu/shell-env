43787d856d4a23ed803cc5e640abf657a58d3e4d
Vu TRAN <trannhatvu94@gmail.com>
2025-11-15 23:12:40 +0100

    feat: shell-env can be cloned anywhere
    Dynamically set SHELL ENV_DIR as the project's source path
    
    Signed-off-by: Vu TRAN <trannhatvu94@gmail.com>

--- main.sh
+++ main.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-for file in ~/shell-env/scripts/*.sh; do
+if [ -n $ZSH_VERSION ]; then
+  SHELL_ENV_DIR="${0:A:h}"
+else
+  SHELL_ENV_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+fi
+
+for file in "$SHELL_ENV_DIR/scripts/"*.sh; do
    [[ -f $file ]] && source "$file"
 done
 
